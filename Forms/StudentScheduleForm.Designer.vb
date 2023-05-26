aa1eb3d1d6fa70ae0379672d7886608b64aa8da5
jeh-arr <106667702+jeh-arr@users.noreply.github.com>
2023-05-26 09:15:00 +0800

    update

--- Forms/StudentScheduleForm.Designer.vb
+++ Forms/StudentScheduleForm.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class StudentGradesForm
+Partial Class StudentScheduleForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,9 +23,6 @@ Partial Class StudentGradesForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
         Me.ButtonBack = New Guna.UI2.WinForms.Guna2Button()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -33,19 +30,9 @@ Partial Class StudentGradesForm
         Me.Guna2PictureBox1 = New Guna.UI2.WinForms.Guna2PictureBox()
         Me.Guna2Panel2 = New Guna.UI2.WinForms.Guna2Panel()
         Me.MainPanel = New Guna.UI2.WinForms.Guna2Panel()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.dgv1st1st = New Guna.UI2.WinForms.Guna2DataGridView()
-        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.Guna2Panel1.SuspendLayout()
         CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Guna2Panel2.SuspendLayout()
-        Me.MainPanel.SuspendLayout()
-        Me.GroupBox1.SuspendLayout()
-        CType(Me.dgv1st1st, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Guna2Panel1
@@ -82,9 +69,9 @@ Partial Class StudentGradesForm
         Me.Label1.ForeColor = System.Drawing.Color.White
         Me.Label1.Location = New System.Drawing.Point(177, 15)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(361, 45)
+        Me.Label1.Size = New System.Drawing.Size(165, 45)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "STUDENT GRADES"
+        Me.Label1.Text = "FORM 1"
         '
         'Guna2BorderlessForm1
         '
@@ -96,7 +83,7 @@ Partial Class StudentGradesForm
         '
         Me.Guna2PictureBox1.BackColor = System.Drawing.Color.Transparent
         Me.Guna2PictureBox1.BorderRadius = 45
-        Me.Guna2PictureBox1.Image = Global.Self_Enrollment_System.My.Resources.Resources.gradeiconsmall1
+        Me.Guna2PictureBox1.Image = Global.Self_Enrollment_System.My.Resources.Resources.form1iconsmall2
         Me.Guna2PictureBox1.ImageRotate = 0!
         Me.Guna2PictureBox1.Location = New System.Drawing.Point(12, 12)
         Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
@@ -124,7 +111,6 @@ Partial Class StudentGradesForm
         Me.MainPanel.AutoScroll = True
         Me.MainPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.MainPanel.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.MainPanel.Controls.Add(Me.GroupBox1)
         Me.MainPanel.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.MainPanel.ForeColor = System.Drawing.Color.White
         Me.MainPanel.Location = New System.Drawing.Point(160, 127)
@@ -132,109 +118,7 @@ Partial Class StudentGradesForm
         Me.MainPanel.Size = New System.Drawing.Size(1278, 605)
         Me.MainPanel.TabIndex = 0
         '
-        'GroupBox1
-        '
-        Me.GroupBox1.AutoSize = True
-        Me.GroupBox1.BackColor = System.Drawing.Color.Gray
-        Me.GroupBox1.Controls.Add(Me.dgv1st1st)
-        Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Font = New System.Drawing.Font("Tahoma", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox1.ForeColor = System.Drawing.Color.White
-        Me.GroupBox1.Location = New System.Drawing.Point(34, 41)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(1132, 341)
-        Me.GroupBox1.TabIndex = 8
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "FIRST YEAR:"
-        '
-        'dgv1st1st
-        '
-        DataGridViewCellStyle1.BackColor = System.Drawing.Color.White
-        Me.dgv1st1st.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
-        Me.dgv1st1st.BackgroundColor = System.Drawing.Color.LightGray
-        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
-        DataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
-        DataGridViewCellStyle2.Font = New System.Drawing.Font("Tahoma", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        DataGridViewCellStyle2.ForeColor = System.Drawing.Color.White
-        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
-        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
-        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.dgv1st1st.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
-        Me.dgv1st1st.ColumnHeadersHeight = 42
-        Me.dgv1st1st.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing
-        Me.dgv1st1st.ColumnHeadersVisible = False
-        Me.dgv1st1st.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
-        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
-        DataGridViewCellStyle3.BackColor = System.Drawing.Color.White
-        DataGridViewCellStyle3.Font = New System.Drawing.Font("Tahoma", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        DataGridViewCellStyle3.ForeColor = System.Drawing.Color.White
-        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(255, Byte), Integer))
-        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(CType(CType(71, Byte), Integer), CType(CType(69, Byte), Integer), CType(CType(94, Byte), Integer))
-        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
-        Me.dgv1st1st.DefaultCellStyle = DataGridViewCellStyle3
-        Me.dgv1st1st.GridColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.dgv1st1st.Location = New System.Drawing.Point(29, 98)
-        Me.dgv1st1st.Name = "dgv1st1st"
-        Me.dgv1st1st.RowHeadersVisible = False
-        Me.dgv1st1st.Size = New System.Drawing.Size(1067, 101)
-        Me.dgv1st1st.TabIndex = 2
-        Me.dgv1st1st.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White
-        Me.dgv1st1st.ThemeStyle.AlternatingRowsStyle.Font = Nothing
-        Me.dgv1st1st.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty
-        Me.dgv1st1st.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty
-        Me.dgv1st1st.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty
-        Me.dgv1st1st.ThemeStyle.BackColor = System.Drawing.Color.LightGray
-        Me.dgv1st1st.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.dgv1st1st.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.dgv1st1st.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
-        Me.dgv1st1st.ThemeStyle.HeaderStyle.Font = New System.Drawing.Font("Tahoma", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.dgv1st1st.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White
-        Me.dgv1st1st.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing
-        Me.dgv1st1st.ThemeStyle.HeaderStyle.Height = 42
-        Me.dgv1st1st.ThemeStyle.ReadOnly = False
-        Me.dgv1st1st.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White
-        Me.dgv1st1st.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal
-        Me.dgv1st1st.ThemeStyle.RowsStyle.Font = New System.Drawing.Font("Tahoma", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.dgv1st1st.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.White
-        Me.dgv1st1st.ThemeStyle.RowsStyle.Height = 22
-        Me.dgv1st1st.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(229, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.dgv1st1st.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(CType(CType(71, Byte), Integer), CType(CType(69, Byte), Integer), CType(CType(94, Byte), Integer))
-        '
-        'Column1
-        '
-        Me.Column1.HeaderText = "Grade"
-        Me.Column1.Name = "Column1"
-        Me.Column1.ReadOnly = True
-        '
-        'Column2
-        '
-        Me.Column2.HeaderText = "Title"
-        Me.Column2.Name = "Column2"
-        Me.Column2.ReadOnly = True
-        '
-        'Column3
-        '
-        Me.Column3.HeaderText = "Unit"
-        Me.Column3.Name = "Column3"
-        Me.Column3.ReadOnly = True
-        '
-        'Column4
-        '
-        Me.Column4.HeaderText = "Description"
-        Me.Column4.Name = "Column4"
-        Me.Column4.ReadOnly = True
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Tahoma", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(24, 54)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(278, 29)
-        Me.Label3.TabIndex = 1
-        Me.Label3.Text = "FIRST SEMESTER: (26.0)"
-        '
-        'StudentGradesForm
+        'StudentScheduleForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -243,7 +127,7 @@ Partial Class StudentGradesForm
         Me.Controls.Add(Me.Guna2Panel1)
         Me.Controls.Add(Me.Guna2Panel2)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Name = "StudentGradesForm"
+        Me.Name = "StudentScheduleForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "StudentInfoForm"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
@@ -251,11 +135,6 @@ Partial Class StudentGradesForm
         Me.Guna2Panel1.PerformLayout()
         CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Guna2Panel2.ResumeLayout(False)
-        Me.MainPanel.ResumeLayout(False)
-        Me.MainPanel.PerformLayout()
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
-        CType(Me.dgv1st1st, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -267,11 +146,4 @@ Partial Class StudentGradesForm
     Friend WithEvents ButtonBack As Guna.UI2.WinForms.Guna2Button
     Friend WithEvents MainPanel As Guna.UI2.WinForms.Guna2Panel
     Friend WithEvents Guna2BorderlessForm1 As Guna.UI2.WinForms.Guna2BorderlessForm
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents Label3 As Label
-    Friend WithEvents dgv1st1st As Guna.UI2.WinForms.Guna2DataGridView
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
-    Friend WithEvents Column4 As DataGridViewTextBoxColumn
 End Class
--- Forms/StudentScheduleForm.vb
+++ Forms/StudentScheduleForm.vb
@@ -1,24 +1,24 @@
-﻿Public Class StudentGradesForm
+﻿Public Class StudentScheduleForm
 
-    Private Sub StudentInfoForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MainPanel.BackColor = Color.FromArgb(150, 0, 0, 0)
+    'Private Sub StudentInfoForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    '    MainPanel.BackColor = Color.FromArgb(150, 0, 0, 0)
 
-        'Connect()
+    '    'Connect()
 
 
-        'query = "SELECT * FROM program"
-        'With command
-        '    .CommandText = query
-        '    .Connection = connection
-        '    reader = .ExecuteReader
+    '    'query = "SELECT * FROM program"
+    '    'With command
+    '    '    .CommandText = query
+    '    '    .Connection = connection
+    '    '    reader = .ExecuteReader
 
-        'End With
-        ''d.Rows.Clear()
-        'While reader.Read
-        '    dgv1st1st.Rows.Add(reader("GE1"), reader(2), reader(3))
-        'End While
+    '    'End With
+    '    ''d.Rows.Clear()
+    '    'While reader.Read
+    '    '    dgv1st1st.Rows.Add(reader("GE1"), reader(2), reader(3))
+    '    'End While
 
-    End Sub
+    'End Sub
 
 
 End Class
